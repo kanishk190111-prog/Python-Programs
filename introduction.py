286272fd53eb247d2ddcab1d6a756e7255e4a3ce
kanishk190111-prog <kanishk190111@gmail.com>
2026-08-21 22:02:46 +0545

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,26 @@
+#Printing a greeting message
+print("Hello, world!")
+
+#Printing my name
+print("My name is Alex.")
+
+#Printing a fun fact about myself
+#print("I love playing soccer.")
+print("I love playing soccer.")
+
+#Performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
+
+#Print a Sentence
+print("Welcome to the World of Programming!")
+
+#Print a Number
+print(4)
+
+#Next Line after print
+print("Hello Python \n")
+
+#Print all the keywords present in Python
+print("Keywords in Python are....")
+import keyword
+print(keyword.kwlist)
